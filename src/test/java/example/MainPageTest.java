07361878f1a1a765f39d5e848a9f06f39a2f4478
VladimirBudylin <seemons901@yandex.ru>
2025-09-05 15:18:25 +0700

    Initial commit

---
+++ src/test/java/example/MainPageTest.java
@@ -0,0 +1,50 @@
+package example;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import java.time.Duration;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+
+public class MainPageTest {
+    private WebDriver driver;
+
+    @BeforeEach
+    public void setUP() {
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--remote-allow-origins=*");
+        driver = new ChromeDriver(options);
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(3));
+        driver.get("https://www.habr.com/");
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(3));
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("body")));
+    }
+
+    @AfterEach
+    public void tearDown() {
+        driver.quit();
+    }
+
+    @Test
+    @DisplayName(value = "Отображение ---")
+    public void burgerTest() {
+        WebElement allStreamsButton = driver.findElement(By.xpath("//a[contains(text(), 'All streams') and contains(@href, '/articles')]"));
+        allStreamsButton.click();
+
+        WebElement hubsButton = driver.findElement(By.xpath("//a[text()='Hubs' and contains(@href, '/hubs')]"));
+        hubsButton.click();
+
+        assertTrue(driver.findElement(By.cssSelector(".tm-suggest-button")).isDisplayed(), "Suggest new не найдена");
+
+    }
+}
