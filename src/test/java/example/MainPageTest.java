7701ca8ba607e528388261625a4ff0b4feaf724d
VladimirBudylin <seemons901@yandex.ru>
2025-09-08 10:23:35 +0700

    Second test added

--- src/test/java/example/MainPageTest.java
+++ src/test/java/example/MainPageTest.java
@@ -38,7 +38,7 @@ public class MainPageTest {
     }
 
     @Test
-    @DisplayName(value = "Отображение кнопки Suggest")
+    @DisplayName(value = "Первый тест, на отображение кнопки Suggest")
     public void suggestTest() {
         WebElement allStreamsButton = driver.findElement(By.xpath("//a[contains(text(), 'All streams') and contains(@href, '/articles')]"));
         allStreamsButton.click();
@@ -47,8 +47,20 @@ public class MainPageTest {
         hubsButton.click();
 
         WebElement suggest = driver.findElement(By.cssSelector(".tm-suggest-button"));
-
         assertTrue(suggest.isDisplayed(), "Suggest new не найдена");
 
+    }
+
+    @Test
+    @DisplayName(value = "Второй тест на отображение пагинации")
+    public void secondTest() {
+        WebElement startupButoon = driver.findElement(By.xpath("//a[@class='footer-menu__item-link' and @href='/en/sandbox/']"));
+        startupButoon.click();
+
+        WebElement paginationButton = driver.findElement(By.xpath("//a[@class='tm-pagination__page' and @href='/en/sandbox/page4/']\n"));
+        paginationButton.click();
+
+        WebElement returnButton = driver.findElement(By.xpath("//span[@class='tm-pagination__navigation-link-title' and text()='Here']\n"));
+        assertTrue(returnButton.isDisplayed(), "Кнопка назад отсутствует");
     }
 }
